bd14dcd70a445a8c4d39646ad9680d37acd12a3e
Bastoon577-lang <bastoon577@gmail.com>
2026-09-18 10:23:07 +0200

    FEAT : Bump version v1-1-0

--- SOFTWARE/Module_TIC/common_utils.h
+++ SOFTWARE/Module_TIC/common_utils.h
@@ -6,7 +6,7 @@
 #include "hal_utils.h"
 
 //< MACRO de version logicielle
-#define V_LOGICIEL                "v1.0.2"              // Version Logicielle
+#define V_LOGICIEL                "v1.1.0"              // Version Logicielle
 
 //< MACRO du Mode Access Point
 #define AP_SSID                   "ModuleTIC"           // SSID ModuleTIC Access Point
